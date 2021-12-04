da1ba19c4448f784bd945e1cc271e1418f5f5e4b
Albin <a.bramstang@gmail.com>
2021-12-04 12:44:40 +0100

    Day 3 part 2

--- Day3/Day3.fs
+++ Day3/Day3.fs
@@ -2,9 +2,8 @@
 
 let getBits (sequence: string) =
     sequence.ToCharArray()
-    |> Seq.map (fun x -> x |> string)
-    |> Seq.map (fun x -> x |> int)
-    |> Seq.toArray
+    |> Array.map (fun x -> x |> string)
+    |> Array.map (fun x -> x |> int)
 
 let toBit middle number =
     if number > middle then 1 else 0
@@ -28,20 +27,33 @@ let solve (data: List<string>) =
         for i in 0..(bitCount - 1) do
             sums.[i] <- sums.[i] + bits.[i]
 
-    let gamma =
-        sums
-        |> Seq.map (toBit (rows / 2))
-        |> Seq.toArray
-    let epsilon =
-        gamma
-        |> Seq.map (fun x -> x - 1 |> abs)
-        |> Seq.toArray
-
+    let gamma = Array.map (toBit (rows / 2)) sums
+    let epsilon = Array.map (fun x -> x - 1 |> abs) gamma
     (toDecimal gamma) * (toDecimal epsilon)
 
-let rec search (position: int) = 
-    0
+let giveList (zeros: List<'a>) (ones: List<'a>) (getLargest: bool) =
+    if zeros.Length = ones.Length then
+        if getLargest then ones else zeros
+    else
+        let (large, small) = if zeros.Length > ones.Length then (zeros, ones) else (ones, zeros)
+        if getLargest then large else small
+
+let rec search (numbers: List<int[]>) (position: int) (mostCommon: bool) = 
+    if numbers.Length = 1 then
+        numbers.Head
+    else
+        let mutable zeroList = []
+        let mutable oneList = []
+    
+        numbers |> List.iter (fun n ->
+            if n.[position] = 0 then zeroList <- n :: zeroList
+            else oneList <- n :: oneList)
+
+        let nextNumbers = giveList zeroList oneList mostCommon
+        search nextNumbers (position + 1) mostCommon
 
 let solve2 (data: List<string>) =
-    let bitList = Seq.map getBits data
-    0
+    let numbers = List.map getBits data
+    let o2 = search numbers 0 true
+    let co2 = search numbers 0 false
+    (toDecimal o2) * (toDecimal co2)
