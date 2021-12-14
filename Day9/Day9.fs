5698329c361e5d26104b4b96fa656c10a22cb30a
Albin <a.bramstang@gmail.com>
2021-12-14 22:25:27 +0100

    Prepare for part 2

--- Day9/Day9.fs
+++ Day9/Day9.fs
@@ -6,8 +6,7 @@ let isLocalMin (point: int) (neighbors: int list) =
     neighbors |> List.forall (fun x -> x > point)
 
 
-let solve (data: string list) =
-    let map = array2D data |> Array2D.map (fun x -> x |> string |> int)
+let getLowPoints (map: int[,]) =
     let rows = Array2D.length1 map
     let cols = Array2D.length2 map
     let mutable lowPoints = []
@@ -26,12 +25,27 @@ let solve (data: string list) =
                 |> List.map (fun x -> x.Value)
             
             if isLocalMin point neighbors then
-                lowPoints <- point :: lowPoints
+                lowPoints <- (point, i, j) :: lowPoints
     
     lowPoints
-    |> List.map (fun x -> x + 1)
+
+
+let solve (data: string list) =
+    let map = array2D data |> Array2D.map (fun x -> x |> string |> int)
+    getLowPoints map
+    |> List.map (fun (x, _, _) -> x + 1)
     |> List.sum
 
 
+let getBasin (map: int[,]) (row: int) (column: int) =
+    []
+
+
 let solve2 (data: string list) =
-    0
+    let map = array2D data |> Array2D.map (fun x -> x |> string |> int)
+    let lowPoints = getLowPoints map
+    let basins = lowPoints |> List.map (fun (_, r, c) -> getBasin map r c)
+    basins
+    |> List.sortByDescending (fun x -> x.Length)
+    |> List.take 3
+    |> List.fold (fun product list -> product * list.Length) 1
