6bce3fe2448238d1e4ecd5520a17a07b5e04434d
Albin <a.bramstang@gmail.com>
2021-12-23 16:07:49 +0100

    Cleanup

--- Day12/Program.fs
+++ Day12/Program.fs
@@ -5,11 +5,6 @@ module Program =
 
     [<EntryPoint>]
     let main args =
-        
-        let lala = [ 1; 1; 1; 2; 2; 3 ]
-        let lala2 = lala |> List.groupBy (fun x -> x)
-        
-        
         let input = File.ReadLines "Day12Part1.txt" |> Seq.toList
         let result = Solver.solve input
         printfn "Part 1: %i" result
