7552492272ace14e4033a9aba04b3152a4bb228b
Albin <a.bramstang@gmail.com>
2021-12-27 23:39:41 +0100

    Slight improvements, still wrong.

--- Day15/Day15.fs
+++ Day15/Day15.fs
@@ -3,12 +3,13 @@
 open Utils
 open System
 
+[<CustomComparison; CustomEquality>]
 type Path =
     struct
         val x: int
         val y: int
         val cost: int
-        val path: (int * int) list
+        val path: Set<(int * int)>
 
         new (x, y, cost, path) =
             {
@@ -17,18 +18,42 @@ type Path =
                 cost = cost
                 path = path
             }
+
+        interface IComparable with
+            member this.CompareTo(other: obj): int = 
+                match other with
+                | :? Path as p ->
+                    if this.GetHashCode() = p.GetHashCode() then
+                        0
+                    elif this.cost < p.cost then
+                        -1
+                    else
+                        1
+                | _ -> -1
+        
+        interface IEquatable<Path> with
+            member this.Equals other = this.GetHashCode() = other.GetHashCode()
+
+        override this.Equals other =
+            match other with
+            | :? Path as p -> this.Equals p
+            | _ -> false
+
+        override this.GetHashCode () =
+            this.x * 3 + this.y * 5 + this.cost * 11
+            
     end
 
 
 module Solver =
     let shortestPath (map: int[,]) (paths: Path list) =
-        let mutable path = paths.Head
-        let mutable paths = paths
+        let mutable pathSet = Set.ofList paths
+        let mutable path = pathSet.MinimumElement
         let goal = (Array2D.length2 map - 1, Array2D.length1 map - 1)
         
         while (path.x, path.y) <> goal do
             let timestamp = DateTime.Now
-            if timestamp.Second = 0 then printfn "Paths: %i, shortest: %i, x: %i, y: %i" paths.Length path.cost path.x path.y 
+            if timestamp.Second = 0 then printfn "Paths: %i, shortest: %i, x: %i, y: %i" pathSet.Count path.cost path.x path.y 
 
             let newSteps =
                 [
@@ -39,14 +64,12 @@ module Solver =
                 ]
                 |> List.filter (fun (cost, x, y) ->
                     cost.IsSome
-                    && path.path |> List.contains (x, y) |> not)
-                |> List.map (fun (cost, x, y) -> Path(x, y, path.cost + cost.Value, (x, y) :: path.path))
+                    && path.path.Contains (x, y) |> not)
+                |> List.map (fun (cost, x, y) -> Path(x, y, path.cost + cost.Value, path.path.Add (x, y)))
             
-            paths <-
-                paths.Tail
-                |> List.append newSteps
-                |> List.sortBy (fun path -> path.cost)
-            path <- paths.Head
+            pathSet <- pathSet.Remove path
+            newSteps |> List.iter (fun x -> pathSet <- pathSet.Add x)
+            path <- pathSet.MinimumElement
 
         path
 
@@ -58,8 +81,8 @@ module Solver =
             |> Array2D.map (fun x -> x |> string |> int)
         let seed =
             [
-                Path(1, 0, map.[0, 1], [ (0, 0); (1, 0) ]);
-                Path(0, 1, map.[1, 0], [ (0, 0); (0, 1) ])
+                Path(1, 0, map.[0, 1], Set.ofList [ (0, 0); (1, 0) ]);
+                Path(0, 1, map.[1, 0], Set.ofList [ (0, 0); (0, 1) ])
             ]
         let shortestPath = shortestPath map seed
         shortestPath.cost
